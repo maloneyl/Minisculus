3530712b36f94de49b3ef7419a9a7414c80ea830
maloneyl <m@thinkmaloney.com>
2014-04-10 12:43:56 +0100

    tested multi-char input as well

--- spec/mark_i_spec.rb
+++ spec/mark_i_spec.rb
@@ -14,6 +14,12 @@ describe MarkI do
       subject(:encrypt_input) { mark_i.encrypt("a") }
       it { should eql("f") }
     end
+
+    context 'when given a wheel setting of 6 and an input of "be"' do
+      let(:mark_i) { MarkI.new(6) }
+      subject(:encrypt_input) { mark_i.encrypt("be") }
+      it { should eql("hk") }
+    end
   end
 
 end
